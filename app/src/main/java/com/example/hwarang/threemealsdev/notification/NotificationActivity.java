f36173bf15be87fdf326b9601e710bf03b117fad
Hwarang-Go <qubic98@gmail.com>
2018-05-28 22:01:43 +0900

    add notification file(activity, layout)

---
+++ app/src/main/java/com/example/hwarang/threemealsdev/notification/NotificationActivity.java
@@ -0,0 +1,15 @@
+package com.example.hwarang.threemealsdev.notification;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.hwarang.threemealsdev.R;
+
+public class NotificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification);
+    }
+}
